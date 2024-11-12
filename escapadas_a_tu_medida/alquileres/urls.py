83c214fdaddf8e2c8ad2d1c2791a39f903c12694
PedroPabloSantos <pedsandom@alum.us.es>
2024-11-12 19:32:23 +0100

    feat: filtro avanzado para la busqueda de propiedades

--- escapadas_a_tu_medida/alquileres/urls.py
+++ escapadas_a_tu_medida/alquileres/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('propiedades/', views.listar_propiedades, name='listar_propiedades'),
     path('propiedad/<int:propiedad_id>/reservar/', views.reservar_propiedad, name='reservar_propiedad'),
-    path('buscar/', views.buscar_alojamiento, name='buscar'),
+    path('buscar/', views.buscar, name='buscar'),
+    path('buscar/filtros', views.buscar_alojamientos, name='buscar_alojamientos'),
     path('gestionPropiedad/', views.crear_propiedad, name='crear_propiedad'),
-
 ]
