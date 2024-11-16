959e58b898acd124dfc5896242d25a079ee2d2ba
angelneria <angneraca@alum.us.es>
2024-11-16 18:06:26 +0100

    Visualizacion de disponibilidad

--- escapadas_a_tu_medida/alquileres/urls.py
+++ escapadas_a_tu_medida/alquileres/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('reservar/<int:propiedad_id>/', views.crear_reserva, name='crear_reserva'),
     path('buscar/', views.buscar_alojamientos, name='buscar_alojamientos'),
-    path('gestionPropiedad/', views.crear_propiedad, name='crear_propiedad'),
+    path('gestionPropiedad/create', views.crear_propiedad, name='crear_propiedad'),
+    path('gestionPropiedad/delete/<int:propiedad_id>/', views.eliminar_propiedad, name='eliminar_propiedad'),
+    path('gestionPropiedad/update/<int:propiedad_id>/', views.actualizar_propiedad, name='editar_propiedad'),
+    path('gestionPropiedad/', views.listar_propiedades_propietario, name='listar_propiedades_propietario')
 ]
