6d6d5770b768be642041206d04cc199352c788d6
angelneria <angneraca@alum.us.es>
2024-11-16 18:13:17 +0100

    Correcion menor

--- escapadas_a_tu_medida/alquileres/urls.py
+++ escapadas_a_tu_medida/alquileres/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('gestionPropiedad/create', views.crear_propiedad, name='crear_propiedad'),
     path('gestionPropiedad/delete/<int:propiedad_id>/', views.eliminar_propiedad, name='eliminar_propiedad'),
     path('gestionPropiedad/update/<int:propiedad_id>/', views.actualizar_propiedad, name='editar_propiedad'),
-    path('gestionPropiedad/', views.listar_propiedades_propietario, name='listar_propiedades_propietario')
+    path('gestionPropiedad/', views.listar_propiedades_propietario, name='listar_propiedades_propietario'),
     path('historialReservas/', views.historial_reservas, name='historial_reservas'),
 ]
