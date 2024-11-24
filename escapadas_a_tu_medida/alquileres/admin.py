91d97e754fb3020ef289ebab154be29333753e8b
angelneria <angneraca@alum.us.es>
2024-11-24 14:26:55 +0100

    Añadido valoracion y nuevos filtrados

--- escapadas_a_tu_medida/alquileres/admin.py
+++ escapadas_a_tu_medida/alquileres/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from alquileres.models import Imagen, Propiedad, Reserva, Disponibilidad, PropiedadesDeseadas
+from alquileres.models import Imagen, Propiedad, Reserva, Disponibilidad, PropiedadesDeseadas, Valoracion
 
 
 admin.site.register(Reserva)
@@ -7,5 +7,6 @@ admin.site.register(Imagen)
 admin.site.register(Propiedad)
 admin.site.register(Disponibilidad)
 admin.site.register(PropiedadesDeseadas)
+admin.site.register(Valoracion)
 
 # Register your models here.
--- escapadas_a_tu_medida/alquileres/urls.py
+++ escapadas_a_tu_medida/alquileres/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('reservar/<int:propiedad_id>/', views.crear_reserva, name='crear_reserva'),
     path('confirmar_reserva/<int:propiedad_id>/', views.confirmar_reserva, name='confirmar_reserva'),
     path('pagoRealizado/', views.pago_realizado, name='pago_realizado'),
-
     path('buscar/', views.buscar_alojamientos, name='buscar_alojamientos'),
     path('gestionPropiedad/create', views.crear_propiedad, name='crear_propiedad'),
     path('gestionPropiedad/delete/<int:propiedad_id>/', views.eliminar_propiedad, name='eliminar_propiedad'),
@@ -21,4 +20,5 @@ urlpatterns = [
     path('agregarListaDeseos/<int:propiedad_id>/', views.agregar_propiedad_deseada, name='agregar_lista_deseos'),
     path('listaDeseos/', views.obtener_lista_deseos, name='lista_deseos'),
     path('eliminarListaDeseos/<int:propiedad_id>/', views.eliminar_de_lista_deseos, name='eliminar_lista_deseos'),
+    path('valorarPropiedad/<int:propiedad_id>/', views.valorar_propiedad, name='valorar_propiedad'),
 ]
